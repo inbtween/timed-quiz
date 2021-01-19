1a751be6c703543b25e3bd03228fffe04f688e76
Michelle Smith <smith.michelle.a@gmail.com>
2021-01-18 23:55:35 -0500

    added Question 1

---
+++ script.js
@@ -0,0 +1,17 @@
+// start button
+// THEN a timer starts
+// presented with a question 1
+// WHEN I answer a question, submit
+
+// presented with a question 2
+// WHEN I answer a question, submit
+
+// if WHEN I answer a question incorrectly
+// HEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+
+// THEN the game is over
+
+// WHEN the game is over
+// THEN I can save my initials and my score
